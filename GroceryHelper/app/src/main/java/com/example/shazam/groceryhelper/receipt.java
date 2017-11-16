2b6cde81f012991493365f57402ea3e035c62040
danieln5004 <danieln816@yahoo.com>
2017-11-15 18:54:40 -0600

    Database added

--- GroceryHelper/app/src/main/java/com/example/shazam/groceryhelper/receipt.java
+++ GroceryHelper/app/src/main/java/com/example/shazam/groceryhelper/receipt.java
@@ -5,9 +5,47 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.graphics.Color;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Context;
+import android.support.v7.app.ActionBar;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.ReferenceQueue;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class receipt extends AppCompatActivity {
+
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMddyyyy", Locale.getDefault());
+
+    RequestQueue queue;
+    Button submitButton;
+    EditText editText;
+    String selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +54,103 @@ public class receipt extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        queue = Volley.newRequestQueue(this);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
+
+        submitButton = (Button)findViewById(R.id.Submit_transaction);
+        editText = (EditText)findViewById(R.id.amount_spent);
+
+        compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        Event ev1 = new Event(Color.RED, 1477040400000L, "Went to the Store");
+        compactCalendar.addEvent(ev1);
+
+        selectedDate = dateFormatMonth.format(new Date());
+
+
+
+        //post request for button, button sends data
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String url = "https://cs449-grocerystore.herokuapp.com/spent/";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("RESPONSE", response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/x-www-form-urlencoded";
+                    }
+
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        Log.d("RESPONSE", "DATE: " + selectedDate + " AMOUNT: " + editText.getText().toString());
+                        params.put("date", selectedDate);
+                        params.put("amount", editText.getText().toString());
+                        return params;
+                    }
+                };
+
+                queue.add(stringRequest);
+            }
+        });
+        //click on date to get request, clicking on date gets request
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                final Context context = getApplicationContext();
+
+                selectedDate = dateFormatMonth.format(Date.parse(dateClicked.toString()));
+                String url = "https://cs449-grocerystore.herokuapp.com/spent/" + selectedDate;
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        Log.d("RESPONSE", response.toString());
+                        try {
+                            Toast.makeText(context, response.getString("amount"), Toast.LENGTH_SHORT).show();
+                        }
+                        catch (JSONException e) {
+                            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                if (dateClicked.toString().compareTo("Fri Oct 21 00:00:00 AST 2016") == 0) {
+                    Toast.makeText(context, "Went to the Store", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Spent:", Toast.LENGTH_SHORT).show();
+                }
+
+                queue.add(jsonObjectRequest);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(dateFormatMonth.format(firstDayOfNewMonth));
+            }
+        });
+
+
+        }
 
 }
